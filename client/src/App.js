9dd591e19df1f51121cbe8b2755c13f1a5414c50
Mourad <mourad777b@gmail.com>
2021-11-21 12:25:56 -0500

    rendering booked dates on calendar

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 import Layout from './hoc/Layout';
 import history from './utility/history';
 import BasicDateRangePicker from './components/date-selector/DateSelector';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios'
 import { DatePicker, Space } from 'antd'
 import moment from 'moment';
 const { RangePicker } = DatePicker;
 function App() {
+
+  const [fromMomentDate, setFromMomentDate] = useState('')
+  const [toMomentDate, setToMomentDate] = useState('')
+  const [adults, setAdults] = useState('')
+  const [children, setChildren] = useState('')
+  const [bookings, setBookings] = useState([])
+
   const setBooking = async () => {
-    return;
+
+
     const response = await axios.post('http://localhost:3001/api/bookings', {
-      checkin: 'today',
-      checkout: 'tommorow'
+      bookingStart: fromMomentDate.format('YYYY-MM-DD HH:mm z'),
+      bookingEnd: toMomentDate.format('YYYY-MM-DD HH:mm z'),
+      roomId: 2,
+      userId: 1
     });
 
     console.log('response', response)
@@ -21,20 +31,27 @@ function App() {
 
   const onDateSelect = (value) => {
     if (!value) return;
-    const fromMomentDate = moment(value[0]);
-    const toMomentDate = moment(value[1]);
-    const fromDate = fromMomentDate.format('DD-MM-YYYY')
-    const toDate = toMomentDate.format('DD-MM-YYYY')
+    setFromMomentDate(moment(value[0]));
+    setToMomentDate(moment(value[1]));
+    const fromDate = moment(value[0]).format('YYYY-MM-DD')
+    const toDate = moment(value[1]).format('YYYY-MM-DD')
     console.log('from date', fromDate)
     console.log('to date', toDate)
-    const duration = moment.duration(toMomentDate.diff(fromMomentDate)).asDays();
-    console.log('duration', duration)
-
+    // const duration = moment.duration(toMomentDate.diff(fromMomentDate)).asDays();
+    // console.log('duration', duration)
+  }
+  
+  const getBookings = async () => {
+    const room = 2;
+    const res = await axios.get(`http://localhost:3001/api/bookings/${room}`)
+    setBookings(res.data)
+    console.log('res',res)
   }
 
-  useEffect(() => {
-    setBooking()
-  }, [])
+  useEffect(()=>{
+    getBookings()
+  },[])
+
   return (
     <Layout>
       <Router history={history}>
@@ -48,11 +65,15 @@ function App() {
           <RangePicker
             onChange={onDateSelect} disabledDate={new Date().getTime()}
             disabledDate={current => {
+              console.log('bookings',bookings)
               const oneDay = 86400000
+              const momentBookingsStart = bookings.map(booking=>moment(booking.bookingStart))
+              const momentBookingsEnd = bookings.map(booking=>moment(booking.bookingEnd))
+              console.log('momentBookingsStart',momentBookingsStart)
               const dateToCompare = moment(new Date().getTime() + oneDay);
               // const today = moment(new Date());
-              console.log('comparison: ',current.startOf('day').isSame(dateToCompare.startOf('day')))
-            return  current.startOf('day').isSame(dateToCompare.startOf('day'));
+              console.log('comparison: ', current.startOf('day').isSame(dateToCompare.startOf('day')))
+              return  [...momentBookingsStart,...momentBookingsEnd].findIndex(bookingDate=>current.startOf('day').isSame(bookingDate.startOf('day'))) > -1;
 
 
               console.log('current', current)
@@ -61,6 +82,7 @@ function App() {
             }}
 
           />
+          <button onClick={setBooking} >Book now!</button>
         </Space>
 
 
--- server/routes/api/bookings.js
+++ server/routes/api/bookings.js
@@ -1,24 +1,47 @@
 const router = require("express").Router();
-const { Booking } = require("../../db/models");
+const { Booking, User, Room } = require("../../db/models");
 const moment = require('moment');
 
 router.post("/", async (req, res, next) => {
   console.log('creating a booking', req.body)
   try {
 
-    const { userId, bookingStart, bookingEnd, checkin, roomId } = req.body;
-    console.log('example time: ', moment(new Date(1637327299335)).format('YYYY-MM-DD'))
+    const { userId, bookingStart, bookingEnd, checkin, roomId, adults, children, message } = req.body;
+    console.log('example time: ', moment(new Date(bookingStart)).format('YYYY-MM-DD'))
     const booking = await Booking.create({
-      userId: 1,
-      bookingStart: moment(new Date(1637327299335)).format('YYYY-MM-DD'),
-      bookingEnd: moment(new Date(1637328299335)).format('YYYY-MM-DD'),
-      checkinTime: moment(new Date(1637397299335)).format('YYYY-MM-DD'),
-      roomId: 2,
+      userId,
+      bookingStart: moment(bookingStart).format('YYYY-MM-DD'),
+      bookingEnd: moment(bookingEnd).format('YYYY-MM-DD'),
+      checkinTime: null,
+      transactionId: 'abc123',
+      adults: 2,
+      children: 0,
+      roomId,
+      message,
     });
 
     res.json({
       booking,
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:roomId", async (req, res, next) => {
+  const room = req.params.roomId;
+  try {
+    const bookings = await Booking.findAll({
+      where: {
+        roomId:room
+      },
+      include: [
+        { model: User, order: ["createdAt", "DESC"] },
+        { model: Room, order: ["createdAt", "DESC"] },
+      ],
+    });
+
+    res.json(bookings);
   } catch (error) {
     next(error);
   }
